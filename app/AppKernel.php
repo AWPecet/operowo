f01275554c55878179607da72b3eab66a4246c73
Lukasz Giza <lukaszgizy@gmail.com>
2012-10-12 08:53:09 +0000

    enabled KnpMenuBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Operowo\Bundle\MainBundle\OperowoMainBundle(),
             new Mopa\Bundle\BootstrapBundle\MopaBootstrapBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
